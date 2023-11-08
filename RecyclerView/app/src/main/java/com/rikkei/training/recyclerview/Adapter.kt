e0318af533e61b4c8faada7efe0b90b4f8b13208
hoangminhdh11 <hoangminhdh11@gmail.com>
2023-11-08 17:40:59 +0700

    recyclerView

--- RecyclerView/app/src/main/java/com/rikkei/training/recyclerview/Adapter.kt
+++ RecyclerView/app/src/main/java/com/rikkei/training/recyclerview/Adapter.kt
@@ -1,29 +1,47 @@
 package com.rikkei.training.recyclerview
 
+import android.app.Activity
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlin.coroutines.coroutineContext
 
 class Adapter: RecyclerView.Adapter<Adapter.MyViewHolder>() {
+    private var listHero = emptyList<Hero>()
+    private lateinit var context: Context
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val imageHero = itemView.findViewById<ImageView>(R.id.image_hero)
         val txtName = itemView.findViewById<TextView>(R.id.text_name)
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentHero = listHero[position]
+        holder.txtName.text = currentHero.name
+
+        Glide.with(context)
+            .load(currentHero.image)
+            .into(holder.imageHero)
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listHero.size
+    }
+
+    fun setData(context: Context, listHero: MutableList<Hero>){
+        this.listHero = listHero
+        this.context = context
     }
 
 
--- RecyclerView/app/src/main/java/com/rikkei/training/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/rikkei/training/recyclerview/MainActivity.kt
@@ -2,10 +2,58 @@ package com.rikkei.training.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.rikkei.training.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = Adapter()
+    private var listHero = mutableListOf<Hero>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        createHeroData()
+        val recyclerView = binding.recyclerView
+//         LinearLayoutManager
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+//        // GridLayoutManager
+//        val layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+//        layoutManager.spanSizeLookup = object : SpanSizeLookup(){
+//            override fun getSpanSize(position: Int): Int {
+//                return when(position % 3){
+//                    0 -> 2
+//                    else -> 1
+//                }
+//            }
+//
+//        }
+
+//        // StaggerGridLayoutManager
+//        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+//        layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
+
+        recyclerView.layoutManager = layoutManager
+        adapter.setData(this, listHero)
+        recyclerView.adapter = adapter
+
+    }
+
+    private fun createHeroData(){
+        listHero.add(Hero("Thor", R.drawable.thor))
+        listHero.add(Hero("Hulk", R.drawable.hulk))
+        listHero.add(Hero("IronMan", R.drawable.ironman))
+        listHero.add(Hero("SpiderMan", R.drawable.spiderman))
+        listHero.add(Hero("Thor", R.drawable.thor))
+        listHero.add(Hero("Hulk", R.drawable.hulk))
+        listHero.add(Hero("IronMan", R.drawable.ironman))
+        listHero.add(Hero("SpiderMan", R.drawable.spiderman))
+
     }
 }
